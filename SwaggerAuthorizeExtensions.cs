704c2206e652581c7b3038003d4e141cc927ad0f
Amir <amir.hossein.khalouei@gmail.com>
2020-07-29 17:41:13 +0430

    dot net core 2.1 supported. username password change supported

--- SwaggerAuthorizeExtensions.cs
+++ SwaggerAuthorizeExtensions.cs
@@ -8,14 +8,14 @@ namespace Swagger.Ui.Extensions
 {
     public static class SwaggerAuthorizeExtensions
     {
-        public static IApplicationBuilder UseSwaggerAuthenticated<T>(this IApplicationBuilder builder) where T : AbstractSwaggerAuthenticationMiddleware
+        public static IApplicationBuilder UseSwaggerAuthenticated<T>(this IApplicationBuilder builder, string username, string password, string swaggerUrlKey = "swagger", string headerName = "Authorization") where T : AbstractSwaggerAuthenticationMiddleware
         {
-            return builder.UseMiddleware<T>();
+            return builder.UseMiddleware<T>(username, password, swaggerUrlKey, headerName);
         }
 
-        public static IApplicationBuilder UseSwaggerAuthenticated(this IApplicationBuilder builder)
+        public static IApplicationBuilder UseSwaggerAuthenticated(this IApplicationBuilder builder, string username, string password, string swaggerUrlKey = "swagger", string headerName = "Authorization")
         {
-            return builder.UseMiddleware<SwaggerBasicAuthMiddleware>();
+            return builder.UseMiddleware<SwaggerBasicAuthMiddleware>(username, password, swaggerUrlKey, headerName);
         }
     }
 }
